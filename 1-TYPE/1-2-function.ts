d94884cff6fa4ba915ba39878502bedf744ba679
byungju0624 <byungju0624@gmail.com>
2021-06-02 20:07:34 +0900

    function modified

--- 1-TYPE/1-2-function.ts
+++ 1-TYPE/1-2-function.ts
@@ -1,34 +1,27 @@
 {
-  //   //JavaScript
-  //   function jsAdd(num1, num2) {
-  //     return num1 + num2;
-  //   }
-  //   //typeSctipt
-  //   function Add(num1: number, num2: number): number {
-  //     return num1 + num2;
-  //   }
-  //   //JavaScript
-  //   function jsFetchNum(id) {
-  //     //code...
-  //     //code...
-  //     //code...
-  //     return new Promise((resolve, reject) => {
-  //       resolve(100);
-  //     });
-  //   }
-
-  //   //TypeScript -> 조금더 나은 문서화 효과를 보여준다.
-  //   function FetchNum(id: string): Promise<number> {
-  //     //code...
-  //     //code...
-  //     //code...
-  //     return new Promise((resolve, reject) => {
-  //       resolve(100);
-  //     });
-  //   }
-
-  //JsvaScript => TypeScript
-  //Optional parameter
+  // // JavaScript
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+  // // typeSctipt
+  // function Add(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+  // // JavaScript
+  // function jsFetchNum(id) {
+  //   //     code... code...     code...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+  // //TypeScript -> 조금더 나은 문서화 효과를 보여준다.
+  // function FetchNum(id: string): Promise<number> {
+  //   //code...     code...     code...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+  // JsvaScript => TypeScript Optional parameter
   function printName(firstName: string, lastName?: string) {
     //정해진 인자 갯수만큼 넘겨줘야한다.
     console.log(firstName);
@@ -46,7 +39,7 @@
 
   //Rest parameter
 
-  function addNumbers(...num: Array<number>) {
+  function addNumbers(...num: Array<number>): number {
     return num.reduce((a, b) => a + b);
   }
   console.log(addNumbers(1, 2, 3, 4, 5, 6));
