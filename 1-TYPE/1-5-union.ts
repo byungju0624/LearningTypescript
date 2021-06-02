1373d8dc6c8568d042516b20fdfdc8916cab7d28
byungju0624 <byungju0624@gmail.com>
2021-06-02 20:44:05 +0900

    union modified

--- 1-TYPE/1-5-union.ts
+++ 1-TYPE/1-5-union.ts
@@ -1,6 +1,6 @@
 {
   /*
-    ** Union Types: OR
+    ** Union Types: OR이해하면 충분하다
     -> 많이 쓰인다. 활용도가 높은 타입
     */
   type Direction = "left" | "right" | "up" | "down"; //모든 가능한 케이스 중에 발생할 수 있는 딱하나를 담을 수 있는 타입을 만들고 싶을때
@@ -10,7 +10,7 @@
   move("left");
 
   type TileSize = 8 | 16 | 32;
-  const tile: TileSize = 8; //위의 3가지 중 하나만 가능하다.
+  const tile: TileSize = 8; //위의 3가지 중 하나만 가능하다. 다른 숫자는 쓰지 못한다. 하나만 할당할 수 있을 때 사용하면 된다.
   type SuccessState = {
     response: {
       body: string;
@@ -30,9 +30,9 @@
   }
   function printLoginState(state: LoginState) {
     if ("response" in state) {
-      console.log(`${state.response.body}`);
+      console.log(`${state.response.body} 🎉`);
     } else {
-      console.log(`${state.reason}`);
+      console.log(`${state.reason} 😂`);
     }
   }
 }
