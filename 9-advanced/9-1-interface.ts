68ab8ec18a8236dd995576242b26ff161458b780
byungju0624 <byungju0624@gmail.com>
2021-07-13 21:59:02 +0900

    advanced

--- 9-advanced/9-1-interface.ts
+++ 9-advanced/9-1-interface.ts
@@ -1,3 +1,9 @@
+/**
+ * interface -> 어떤 것의 규격사항, 다른 사람과 의사소통 할 때, 오브젝트와 오브젝트 간의 의사소통을 할 때 
+                정해진 인터페이스를 토대로 해서 서로 간의 상호작용을 할 수 있도록 도와주는 것, 이런 경우에 타입을 쓰는 것은 좋지 않다. 어떠한 것을 구현할 목적으로는 인터페이스가 좋다.
+ * tpye -> 우리가 어떠한 데이터를 담을 때 어떤 데이터를 담을 수 있을지 이 데이터의 모습, 데이터의 타입을 결정하는 것 데이터를 담을 목적으로는 타입을 쓰는 것이 좋다.
+ */
+
 type PositionType = {
   x: number;
   y: number;
@@ -17,6 +23,7 @@ const obj1: PositionType = {
 const obj2: PositionInterface = {
   x: 1,
   y: 1,
+  z: 1,
 };
 
 //★ class
@@ -29,6 +36,7 @@ class Pos1 implements PositionType {
 class Pos2 implements PositionInterface {
   x: number;
   y: number;
+  z: number;
 }
 
 // Extends 기존의 인터페이스,타입를 확장할 수 있다.
